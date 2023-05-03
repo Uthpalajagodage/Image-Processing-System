fc02b2ecb26c1bf17adcd2388521f8d80bcc6c79
Uthpalajagodage <101706471+Uthpalajagodage@users.noreply.github.com>
2023-05-04 05:26:01 +0530

    update

--- MachineLearning/Task2.R
+++ MachineLearning/Task2.R
@@ -6,22 +6,22 @@ library(MLmetrics)
 library(keras)
 
 #PART 1
-# Define the root-mean-square error (RMSE) function
+# Definition of the root-mean-square error
 rmse <- function(error) {
   return(sqrt(mean(error^2)))
 }
 
-# Define the mean absolute error (MAE) function
+# Definition of the mean absolute error
 mae <- function(error) {
   return(mean(abs(error)))
 }
 
-# Define the mean absolute percentage error (MAPE) function
+# Definition of the mean absolute percentage error
 mape <- function(actual, predicted) {
   return(mean(abs((actual - predicted)/actual)) * 100)
 }
 
-# Define the symmetric mean absolute percentage error (sMAPE) function
+# Definition of the symmetric mean absolute percentage error
 smape <- function(actual, predicted) {
   return(2 * mean(abs(actual - predicted) / (abs(actual) + abs(predicted)))*100)
 }
@@ -29,16 +29,11 @@ smape <- function(actual, predicted) {
 # Load the UOW consumption dataset
 UOW_consumption_data <- read_xlsx("datasets/uow_consumption.xlsx")
 
-
-
-summary(UOW_consumption_data) # Get summary statistics for the column
-
-
-# Extract the hourly electricity consumption data for 20:00 for 2018 and 2019
+# For the years 2018 and 2019, extrapolate the hourly electricity consumption figures for 20:00.
 #   "0.83333333333333337"= 20.00  (20/24 = 0.83333333333333337)
 consumption_hourly_20 <- UOW_consumption_data[c("date", "0.83333333333333337")]
 
-# plot the hourly consumption data for 20:00 for 2018 and 2019
+# Plot the 20:00 hourly consumption data for 2018 and 2019.
 ggplot(UOW_consumption_data, aes(x=date, y=`0.83333333333333337`)) +
   geom_line() +
   labs(title = "Hourly Consumption for 20:00",
@@ -46,11 +41,11 @@ ggplot(UOW_consumption_data, aes(x=date, y=`0.83333333333333337`)) +
        y = "Consumption")
 
 
-# Extract the first 380 samples as training data, and the remaining samples as testing data
+# Extract the first 380 samples for training and the rest for testing.
 trained_data <- unlist(consumption_hourly_20[1:380, "0.83333333333333337"])
 test_dataset <- unlist(consumption_hourly_20[381:nrow(consumption_hourly_20), "0.83333333333333337"])
 
-# plot the first 380 samples as training data
+# The first 380 samples are plotted as training data.
 ggplot(data.frame(trained_data), aes(x=1:length(trained_data), y=trained_data)) +
   geom_line() +
   labs(title = "Training Data",
@@ -58,11 +53,11 @@ ggplot(data.frame(trained_data), aes(x=1:length(trained_data), y=trained_data)) 
        y = "Consumption")
 
 
-# Define the number of time-delayed inputs
+# Determine how many time-delayed inputs there are.
 no_inputs <- 60
 
 
-# Construct the input/output matrix for MLP training/testing
+# Create the input/output matrix for MLP testing and training.
 matrix_input_output <- matrix(0, nrow=length(trained_data)-no_inputs, ncol=no_inputs+1)
 
 for (i in 1:(length(trained_data)-no_inputs)) {
@@ -70,10 +65,10 @@ for (i in 1:(length(trained_data)-no_inputs)) {
   matrix_input_output[i, no_inputs+1] <- trained_data[i+no_inputs]
 }
 
-# Normalize the input/output matrix
+# The input/output matrix be normalized
 matrix_input_output <- apply(matrix_input_output, 2, function(x) (x - mean(x)) / sd(x))
 
-# Define the neural network structure to be evaluated
+# Define the neural network architecture that will be assessed.
 structure <- list(
   c(5),
   c(10),
@@ -95,30 +90,30 @@ result <- list()
 
 for (i in 1:length(structure)) {
   
-  # Train the MLP using the normalized input/output matrix
+  # The normalized input/output matrix is used to train the MLP.
   mlp <- neuralnet(V2 ~ ., data=matrix_input_output, hidden=structure[[i]], linear.output=TRUE)
   
   # #Plot the neural network
   # plot(mlp)
   
-  # Extract the inputs for the test data
+  # Extract the test data's inputs.
   testing_inputs <- matrix(test_dataset[1:(length(test_dataset)-no_inputs)], ncol=no_inputs, byrow=TRUE)
   
-  # Predict the output values for the test data
+  # Predict the test data's output values.
 output_mlp <- predict(mlp, testing_inputs)
   
-  # Denormalize the predicted output values
+  # Denormalize the anticipated results.
 output_mlp <- (mlp_output * sd(trained_data)) + mean(trained_data)
   
-  # Calculate the MAE for the predicted output values and the actual output values
+  # Determine the MAE for the actual output values and the predicted output values.
   result_mae <- mae(mlp_output - test_dataset[(no_inputs+1):length(test_dataset)])
   
   cat("the test performances for c(",structure[[i]],")\n")
   
-  # Print the MAE result
+  # Print MAE result
   cat("The MAE for the test data is:", round(result_mae, 2),"\n")
   
-  # Calculate the RMSE for the predicted output values and the actual output values
+  # Determine the RMSE between the output values that were expected and those that were achieved.
 result_rmse <- rmse(mlp_output - test_dataset[(no_inputs+1):length(test_dataset)])
   
   # Print the RMSE result
@@ -129,24 +124,24 @@ result_rmse <- rmse(mlp_output - test_dataset[(no_inputs+1):length(test_dataset)
     return(mean(abs((actual - predicted)/actual)) * 100)
   }
   
-  # Calculate the MAPE for the predicted output values and the actual output values
+  # Determine the MAPE using the predicted and actual output values.
 result_mape <- mape(test_dataset[(no_inputs+1):length(test_dataset)], output_mlp)
   
   # Print the MAPE result
   cat("The MAPE for the test data is:", round(result_mape, 2),"\n")
   
-  # Define the symmetric mean absolute percentage error (sMAPE) function
+  # The symmetric mean absolute percentage error (sMAPE) function should be defined.
   smape <- function(actual, predicted) {
     return(2 * mean(abs(actual - predicted) / (abs(actual) + abs(predicted))) * 100)
   }
   
-  # Calculate the sMAPE for the predicted output values and the actual output values
+  # Make a SMAPE calculation using the predicted and actual output values.
   result_smape <- smape(test_dataset[(no_inputs+1):length(test_dataset)], mlp_output)
   
   # Print the sMAPE result
   cat("The sMAPE for the test data is:", round(result_smape, 2),"\n\n")
   
-  # Store the result for the current neural network structure
+  # Save the outcome for the current neural network configuration.
   result[[i]] <- c(structure[[i]], result_mae, result_rmse, result_mape, result_smape)
   
 }
@@ -158,7 +153,7 @@ colnames(results_dframe) <- c("Structure", "MAE", "RMSE", "MAPE (%)", "sMAPE (%)
 # Print the comparison table of testing performances
 print(results_dframe)
 
-# Find the best one-hidden and two-hidden layer structure based on MAE and total number of weights
+# Based on MAE and the total number of weights, choose the best one-hidden and two-hidden layer structure.
 best_one_hidden <- results_dframe[which.min(results_dframe$MAE & results_dframe$Structure),]
 best_two_hidden <- results_dframe[which.min(results_dframe$MAE & results_dframe$Structure),]
 
@@ -175,49 +170,49 @@ cat("The best two-hidden layer neural network structure is",
 #Part 2
 
 
-# Define a function to build a neural network model
+# Define a procedure for constructing a neural network model.
 build_neural_network <- function(trained_data, test_dataset, input_vars, hidden_structure) {
   
-  # Create formula for the neural network
+  # Make a neural network formula.
   formula <- paste("hour_20 ~", paste(input_vars, collapse = " + "))
   
-  # Build the neural network model using the neuralnet package
+  # Create the neural network model utilizing the neuralnet package.
  model_nn <- neuralnetwork(as.formula(formula), trained_data, hidden = hidden_structure)
   
-  # Prepare the test data for prediction
+  # The test data for the prediction
 matrix_test <- as.matrix(test_dataset[, input_vars, drop = FALSE])
   colnames(matrix_test) <- colnames(trained_data[, input_vars, drop = FALSE])
   
-  # Make predictions using the neural network model
+  # Use the neural network model to predict things.
   predictions <- predict(model_nn, matrix_test)
   
-  # Return the neural network model and its predictions
+  # Bring back the neural network model and its forecasts.
   return(list(model = model_nn, predictions = predictions))
 }
 
-# Function to calculate different evaluation metrics
-metrics_calculation <- function(actual_val, predicted_val) {
-  # Calculate Root Mean Squared Error
+  # function to determine various evaluation metrics
+  metrics_calculation <- function(actual_val, predicted_val) {
+  # Determine the error's root mean square
   rmse <- sqrt(mean((actual_val - predicted_val)^2))
   
-  # Calculate Mean Absolute Error
+  # Make a mean absolute error calculation.
   mae <- mean(abs(actual_val - predicted_val))
   
-  # Calculate Mean Absolute Percentage Error
+  # Make a mean absolute percentage error calculation.
   mape <- mean(abs((actual_val - predicted_val) / actual_val)) * 100
   
-  # Calculate Symmetric Mean Absolute Percentage Error
+  # Determine the Symmetric Mean Absolute Percentage Error.
   smape <- mean(abs(actual_val - predicted_val) / (abs(actual_val) + abs(predicted_val)) * 2) * 100
   
-  # Return a list containing all the evaluation metrics
+  # Provide a list including all the evaluation metrics.
   return(list(RMSE = rmse, MAE = mae, MAPE = mape, sMAPE = smape))
 }
 
 
-# Rename columns to be more descriptive
+# Add more descriptive column names
 colnames(UOW_consumption_data) <- c("date", "hour_18", "hour_19", "hour_20")
 
-# Create lagged variables for hour_20, with different time lags
+# Set up delayed variables for hour_20 with various time delays.
 UOW_consumption_data$lag_1 <- lag(UOW_consumption_data$hour_20, 1)
 UOW_consumption_data$lag_2 <- lag(UOW_consumption_data$hour_20, 2)
 UOW_consumption_data$lag_3 <- lag(UOW_consumption_data$hour_20, 3)
@@ -227,43 +222,41 @@ UOW_consumption_data$lag_7 <- lag(UOW_consumption_data$hour_20, 7)
 # Remove rows with missing values
 UOW_consumption_data <- na.omit(UOW_consumption_data)
 
-# Split data into training and testing sets based on row index
+# Divide the data into training and test sets according to row index
 train <- UOW_consumption_data[1:380,]
 test <- UOW_consumption_data[381:nrow(UOW_consumption_data),]
 
-# Define normalization function to scale data between 0 and 1
+# Create a normalization function that scales data between 0 and 1.
 normalized <- function(x) {
   return((x - min(x)) / (max(x) - min(x)))
 }
 
-# Compute range before normalization
+# Range computation before normalization
 before_range <- apply(train[, -1], 2, range)
 
 
 # Normalize the dataset
 UOW_dataset_train_normalized <- apply(train[, -1], 2, normalize)
 
-# Compute range after normalization
+# after normalization, compute the range
 after_range <- apply(UOW_dataset_train_normalized, 2, range)
 
-# Plot range before normalization
+# Range plot before normalization
 plot(before_range, main = "Range Before Normalization", xlab = "Features", ylab = "Range")
 
 
-# Plot range after normalization
+#range of the plot after normalizing
 plot(after_range, main = "Range After Normalization", xlab = "Features", ylab = "Range")
 
-
-
-# Apply normalization function to all columns except the date column in the testing set
+# All columns in the testing set should be normalized, with the exception of the date column.
 UOW_dataset_test_normalized <- apply(test[, -1], 2, normalize)
 
-# Rename columns in the testing set to match the column names in the training set
+# Rename columns in the testing set to correspond with the names of the columns in the training set.
 colnames(UOW_dataset_test_normalized) <- colnames(UOW_dataset_train_normalized)
 
 
-# Add the 18th and 19th hour attributes to the input vectors
-# Define the input vectors as a list of character vectors
+# Add the 18th and 19th attributes to the input vectors
+# Give the input vectors a character vector list definition.
 vectors_narx_input <- list(
   c("lag_1", "hour_18", "hour_19"),
   c("lag_1", "lag_2", "hour_18", "hour_19"),
@@ -273,7 +266,7 @@ vectors_narx_input <- list(
 )
 
 # Build NARX models
-# Define an empty list to store the models
+# Create a list that is empty to hold the models.
 models_narx <- list()
 summary(UOW_dataset_train_normalized)
 summary(UOW_dataset_test_normalized)
@@ -314,21 +307,21 @@ for (i in 1:length(narx_models)) {
   metrics_evaluation[[i]] <- metrics_calculation(UOW_dataset_test_normalized$hour_20, narx_models[[i]]$predictions)
 }
 
-# Add more models with different hidden layer structure and input vectors to create 12-15 models in total
+# Create 12–15 additional models with various hidden layer structures and input vectors.
 
-# Efficiency comparison between one-hidden layer and two-hidden layer networks
+# One-hidden-layer and two-hidden-layer networks are compared in terms of efficiency.
 
-# Build a one-hidden layer neural network
+# Construct a single hidden layer neural network.
 hidden_model_1 <- build_neural_net(UOW_dataset_train_normalized, UOW_dataset_test_normalized, c("lag_1", "hour_18", "hour_19"), c(5))
 
-# Build a two-hidden layer neural network
+# Create a neural network with two hidden layers.
 hidden_model_2 <- build_neural_net(UOW_dataset_train_normalized, UOW_dataset_test_normalized, c("lag_1", "lag_2", "lag_3", "lag_4", "lag_7", "hour_18", "hour_19"), c(3, 2))
 
-# Check the total number of weight parameters per network
+# The total amount of weight parameters per network is ridiculous
 hidden_num_weights_1 <- sum(sapply(hidden_model_1$model$weights, length))
 hidden_num_weights_2 <- sum(sapply(model_2_hidden$model$weights, length))
 
-# Print the number of weight parameters for each network
+# For each network, print the number of weight parameters.
 cat("Total number of weight parameters for the one-hidden layer network:", hidden_num_weights_1, "\n")
 cat("Total number of weight parameters for the two-hidden layer network:", hidden_num_weights_2, "\n")
 
@@ -337,30 +330,28 @@ cat("Total number of weight parameters for the two-hidden layer network:", hidde
 denormalize <- function(x, min_value, max_value) {
   return(x * (max_value - min_value) + min_value)
 }
-
-
-# Find the index of the best model based on the RMSE evaluation metric
+# Based on the RMSE evaluation metric, identify the optimal model's index.
 index_best_model <- which.min(sapply(evaluation_metrics, function(x) x$RMSE))
 
 # Get the best model and its predictions
 # check the length of models list
 length(narx_models)
 
-# set best_model_index to a valid index
+# change best_model_index to a legitimate index
 index_best_model <- 1
 
-# get the best model
+# choose the best model
 best_model <- narx_models[[best_model_index]]
 predictions_best_model <- best_model$predictions
 
-# Find the minimum and maximum values of the 'hour_20' variable in the training set
+# Determine the variable 'hour_20's' minimum and maximum values in the training set.
 min_value <- min(train$hour_20)
 max_value <- max(train$hour_20)
 
-# Denormalize the model predictions using the min and max values of the 'hour_20' variable
+# Denormalize the model predictions using the minimum and maximum values of the 'hour_20' variable.
 predictions_denormalized <- denormalize(predictions_best_model, min_value, max_value)
 
-# Plot the predicted output vs. desired output using a line chart
+# Use a line chart to compare the expected and desired results.
 plot(test$hour_20, type = "l", col = "blue", xlab = "Time", ylab = "Hour 20 Consumption", main = "Line Chart of Desired vs. Predicted Output")
 lines(predictions_denormalized, col = "red")
 legend("topleft", legend = c("Desired Output", "Predicted Output"), col = c("blue", "red"),lty=1,cex=0.8)
